0fe8327f19113fdb51c3421db0e0e71100954eb3
Vishwas Siravara <vsiravara@gmail.com>
2023-09-21 11:20:29 -0700

    cdk bug bash
    Signed-off-by: Vishwas Siravara <vsiravara@gmail.com>

--- lib/cdk-bug-bash-stack.ts
+++ lib/cdk-bug-bash-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { BundlingFileAccess } from 'aws-cdk-lib';
 
 export class CdkBugBashStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const entry = path.join(__dirname, 'python-lambda');
+    new PythonFunction(this, 'MyFunction', {
+      entry: entry, // required
+      runtime: Runtime.PYTHON_3_8, // required
+      index: 'index.py', // optional, defaults to 'index.py'
+      handler: 'handler', // optional, defaults to 'handler'
+      bundling: {
+        assetExcludes: ['.ignorefile'],
+      }
+    });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBugBashQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const e = path.join(__dirname, 'python-lambda-custom')
+    new PythonFunction(this, 'CustomBuild', {
+      entry: e, // required
+      runtime: Runtime.PYTHON_3_8, // required
+      index: 'index.py', // optional, defaults to 'index.py'
+      handler: 'handler', // optional, defaults to 'handler'
+      bundling: {
+        image: cdk.DockerImage.fromBuild(path.join(entry)),
+        bundlingFileAccess: BundlingFileAccess.VOLUME_COPY,
+        assetExcludes: ['.ignorefile'],
+      }
+    }); 
   }
 }
